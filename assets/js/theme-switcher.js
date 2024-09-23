7cc79dcbcad84369d6e3403e2515feb196a8c81b
dev04 <saltacloud.dev04@gmail.com>
2024-09-23 19:12:07 -0300

    Commit provisorio

---
+++ assets/js/theme-switcher.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){   
+    (function () {
+        window.onload = function () {
+            const preloader = document.querySelector(".page-loading");
+            preloader.classList.remove("active");
+            setTimeout(function () {
+                preloader.remove();
+            }, 1000);
+        };
+    })();
+
+    
+})
+    
+
